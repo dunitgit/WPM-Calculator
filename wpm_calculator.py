813527fbb0c9fce3243553d44124a8ae51ded942
Dunit <dunit@protonmail.com>
2018-09-24 18:26:35 +0200

    Added global menu (curses) + refactored logic

--- wpm_calculator.py
+++ wpm_calculator.py
@@ -1,66 +1,108 @@
+#!/usr/bin/python3
+
 """
 Words per minute calculator.
 """
 
 import curses
-from curses import wrapper
 import time
 import wikipedia
 
 def main(stdscr):
     """ Program entry point """
-    stdscr = curses.initscr() # create a window object that represents the terminal window
-    curses.noecho() # Don't print what I type on the terminal
-    curses.cbreak() # React to every key press, not just when pressing "enter"
-    stdscr.keypad(True) # Enable easy key codes (will come back to this)
-    curses.curs_set(0) # Curser get outta here!
-    run(stdscr, get_text(3, 0))
-
-def print_wpm(stdscr, count, start, end):
-    """ Print WPM """
-    if end - start != 0:
-        stdscr.addstr('Word count: ' + str(count) + '\n')
-        stdscr.addstr('WPM: ' + str(int(round((count / (end - start)) * 60))))
-        stdscr.getch()
-
-def run(stdscr, text):
-    """ Run the WPM calculator """
-    text_len = len(text) # text length
-    word_count = len(text.split(' ')) # word count
-    char_counter = 0 # Progress counter
-    first_char = True # First char hit flag
-    terminate = False # Terminate flag
-
+    # Don't print what is typed in the terminal
+    curses.noecho()
+    # React to every key press, not just when pressing "enter"
+    curses.cbreak()
+    # Enable easy key codes
+    stdscr.keypad(True)
+    # Remove cursor
+    curses.curs_set(0)
+    # Clear screen
+    stdscr.clear()
+    # Refresh screen
+    stdscr.refresh()
+    # Init colors
+    curses.start_color()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    # Get wikipedia text
+    text = get_text(3, 0)
 
-    stdscr.addstr(text[char_counter], curses.color_pair(1))
-    stdscr.addstr(text[1:])
+    # User input
+    key = 266
+    # Text variables
+    text = ''
+    text_len = 0
+    word_count = 0
+    char_counter = 0
+    first_char = True
 
-    for text_char in text:
-        while not terminate:
-            key_press = stdscr.getkey()
-            if key_press == 'å': # User terminated program
-                terminate = True
-            if key_press == text_char: # User hit the correct key
-                if first_char: # Start counting when the user hit the first char
-                    start_time = time.time()
-                    first_char = False
-                if char_counter == text_len - 1: # Stop counting when the user hit the last char
-                    end_time = time.time()
-                    terminate = True
-                    break
-                char_counter += 1 # Update char counter and visual progress
-                stdscr.clear()
-                stdscr.addstr(text[0:char_counter], curses.color_pair(2))
-                stdscr.addstr(text[char_counter], curses.color_pair(1))
-                stdscr.addstr(text[char_counter + 1: text_len])
-                break
-
-    # If the user did not complete dont print summary
-    if end_time != 0:
+    # Run until user press 'ESC'
+    while key != 27:
+        # Clear screen
         stdscr.clear()
-        print_wpm(stdscr, word_count, start_time, end_time)
+        # Check for specific user inputs
+        # F1 (Restart)
+        if key == 265:
+            char_counter = 0
+            first_char = True
+            start_time = 0
+            end_time = 0
+        # F2 (New text)
+        if key == 266:
+            text = get_text(1, 0)
+            text_len = len(text)
+            word_count = len(text.split(' '))
+            char_counter = 0
+            first_char = True
+            start_time = 0
+            end_time = 0
+
+        # User hit the correct key
+        if key == ord(text[char_counter]):
+            # Start timer when the user hit the first character
+            if first_char:
+                start_time = time.time()
+                first_char = False
+            # Check if done
+            if char_counter == text_len - 1:
+                end_time = time.time()
+                stdscr.clear()
+                key = render_scorescreen(stdscr, word_count, start_time, end_time)
+                continue
+            # Update character counter
+            char_counter += 1
+
+        render_text(stdscr, text, text_len, char_counter)
+        render_bottom_menu(stdscr)
+
+        stdscr.refresh()
+        key = stdscr.getch()
+
+def render_text(stdscr, text, text_len, char_counter):
+    """ Render text """
+    stdscr.addstr(text[0:char_counter], curses.color_pair(2))
+    stdscr.addstr(text[char_counter], curses.color_pair(1))
+    stdscr.addstr(text[char_counter + 1: text_len])
+
+def render_bottom_menu(stdscr):
+    """ Render bottom menu """
+    bottom_menu = "Restart: 'F1' | New text: 'F2' | Quit: 'ESC'"
+    height, width = stdscr.getmaxyx()
+    stdscr.attron(curses.color_pair(3))
+    stdscr.addstr(height-1, 0, bottom_menu)
+    stdscr.addstr(height-1, len(bottom_menu), " " * (width - len(bottom_menu) - 1))
+    stdscr.attroff(curses.color_pair(3))
+
+def render_scorescreen(stdscr, count, start, end):
+    """ Render scorescreen """
+    stdscr.addstr('Word count: ' + str(count) + '\n')
+    stdscr.addstr('WPM: ' + str(int(round((count / (end - start)) * 60))) + '\n\n')
+    stdscr.addstr('Press any key to play again...' + '\n')
+    render_bottom_menu(stdscr)
+    return stdscr.getch()
 
 
 def get_text(max_tries, count):
@@ -77,4 +119,4 @@ def get_text(max_tries, count):
         page = get_text(max_tries, count + 1)
     return page
 
-wrapper(main)
+curses.wrapper(main)
